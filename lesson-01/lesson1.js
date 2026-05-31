34682b86ac3d8a4916774d78c4f85f9e5eac4e81
Alina Chumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2026-05-31 18:39:13 +0300

    Lesson 1: variables and types

---
+++ lesson-01/lesson1.js
@@ -0,0 +1,9 @@
+const firstName = "Alina";
+const age = 38;
+const city = "Odesa";
+let isWorkIt = true;
+
+console.log(`Мене звати ${firstName}`);
+console.log(`Мені ${age} років`);
+console.log(`Я живу в ${city}`);
+console.log(`Працювала в IT: ${isWorkIt}`);
---
+++ lesson-01/types.js
@@ -0,0 +1,13 @@
+const firstName = "Alina";
+const age = 38;
+const city = "Odesa";
+const isAngularDeveloper = false;
+const phone = null;
+let salary;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof isAngularDeveloper);
+console.log(typeof phone);
+console.log(typeof salary);         
